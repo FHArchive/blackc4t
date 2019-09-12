8aa6092d396bbe6a2be6d26f6190c3d986e1996e
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2019-09-12 20:21:27 +0100

    sw

---
+++ sw.js
@@ -0,0 +1,23 @@
+﻿const cacheVersion = "blackc4t";
+const urlsToPrefetch = [
+  "/PWA.BlackC4t/"
+];
+
+
+
+this.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheVersion).then(function(cache) {
+      return cache.addAll(urlsToPrefetch);
+    })
+  );
+});
+
+
+this.addEventListener('fetch', event => {
+  let responsePromise = caches.match(event.request).then(response => {
+    return response || fetch(event.request)
+  });
+
+  event.respondWith(responsePromise);
+});
