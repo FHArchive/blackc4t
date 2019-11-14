eadeec2cf2eb394479ffcbaa3cc7f6a0c22c04a8
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2019-11-14 20:02:05 +0000

    update sw

--- sw.js
+++ sw.js
@@ -1,6 +1,8 @@
-﻿const cacheVersion = "BlackC4t-2019.11.3";
+﻿const cacheVersion = "BlackC4t-2019.11.4";
 const urlsToPrefetch = [
 	"/PWA.BlackC4t/",
+	"/PWA.BlackC4t/manifest.json",
+	"/PWA.BlackC4t/images/appicons/squircle-256.png",
 
 	"/PWA.BlackC4t/index.html",
 	"/PWA.BlackC4t/2kotp.html",
@@ -11,7 +13,6 @@ const urlsToPrefetch = [
 	"/PWA.BlackC4t/scripts/password.js",
 	"/PWA.BlackC4t/scripts/public.js",
 
-
 	"/css/theme/auto.css",
 	"/css/theme/black.css",
 	"/css/theme/dark.css",
@@ -21,13 +22,8 @@ const urlsToPrefetch = [
 	"/scripts/navbar.js",
 	"/scripts/script.js",
 	"/scripts/settings.js",
-	"/images/pageicons/info.svg",
-	"/images/pageicons/settings.svg",
-	"/images/pageicons/keyboard_backspace.svg",
-	"/images/appicons/squircle-256.png"
+	"/fonts/FiraSansNF.woff2"
 ];
-
-
 
 self.addEventListener("install", function (event) {
 	event.waitUntil(
@@ -37,11 +33,11 @@ self.addEventListener("install", function (event) {
 	);
 });
 
-self.addEventListener("activate", function(event) {
+self.addEventListener("activate", function (event) {
 	event.waitUntil(
-		caches.keys().then(function(keyList){
-			return Promise.all(keyList.map(function(key){
-				if (key !== cacheVersion){
+		caches.keys().then(function (keyList) {
+			return Promise.all(keyList.map(function (key) {
+				if (key !== cacheVersion) {
 					return caches.delete(key);
 				}
 			}));
@@ -50,10 +46,26 @@ self.addEventListener("activate", function(event) {
 	return self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  let responsePromise = caches.match(event.request).then((response) => {
-    return response || fetch(event.request);
-  });
-
-  event.respondWith(responsePromise);
+self.addEventListener('fetch', function (event) {
+	event.respondWith(fromNetwork(event.request, 400).catch(function () {
+		return fromCache(event.request);
+	}));
 });
+
+function fromNetwork(request, timeout) {
+	return new Promise(function (fulfill, reject) {
+		var timeoutId = setTimeout(reject, timeout);
+		fetch(request).then(function (response) {
+			clearTimeout(timeoutId);
+			fulfill(response);
+		}, reject);
+	});
+}
+
+function fromCache(request) {
+	return caches.open(cacheVersion).then(function (cache) {
+		return cache.match(request).then(function (matching) {
+			return matching || Promise.reject('no-match');
+		});
+	});
+}
