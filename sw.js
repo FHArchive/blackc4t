c170ca65a9ba109ee16125814cf619ad669f2719
Kieran W <41634689+FredHappyface@users.noreply.github.com>
2020-02-04 15:25:45 +0000

    updated theme and used build.js
    minor modifications required
    primary: index % 2 === 1 changed to
    primary: index % 2 === 0
    appbar theme shibe for 2kotp.html

--- sw.js
+++ sw.js
@@ -1,29 +1,5 @@
-﻿const CACHE = "BlackC4t";
-const precacheFiles = [
-	"/PWA.BlackC4t/",
-	"/PWA.BlackC4t/manifest.json",
-	"/PWA.BlackC4t/images/appicons/squircle-256.png",
-	"/PWA.BlackC4t/images/page/shiba-inu-wide.jpg",
-
-	"/PWA.BlackC4t/index.html",
-	"/PWA.BlackC4t/2kotp.html",
-	"/PWA.BlackC4t/public.html",
-	"/PWA.BlackC4t/password.html",
-
-	"/PWA.BlackC4t/scripts/otp.js",
-	"/PWA.BlackC4t/scripts/password.js",
-	"/PWA.BlackC4t/scripts/public.js",
-
-	"/css/theme/auto.css",
-	"/css/theme/black.css",
-	"/css/theme/dark.css",
-	"/css/theme/light.css",
-	"/css/main.css",
-	"/css/settings.css",
-	"/scripts/navbar.js",
-	"/scripts/script.js",
-	"/scripts/settings.js",
-	"/fonts/FiraSansNF.woff2"
+const CACHE = "BlackC4t";
+const precacheFiles = ["/PWA.BlackC4t/","/PWA.BlackC4t/manifest.json","/PWA.BlackC4t/images/appicons/squircle-256.png","/PWA.BlackC4t/images/page/shiba-inu-wide.jpg","/PWA.BlackC4t/index.html","/PWA.BlackC4t/2kotp.html","/PWA.BlackC4t/public.html","/PWA.BlackC4t/password.html","/PWA.BlackC4t/scripts/otp.js","/PWA.BlackC4t/scripts/password.js","/PWA.BlackC4t/scripts/public.js","/css/theme/auto.css","/css/theme/black.css","/css/theme/dark.css","/css/theme/light.css","/css/main.css","/css/settings.css","/scripts/navbar.js","/scripts/script.js","/scripts/settings.js","/fonts/FiraSansNF.woff2",
 ];
 
 self.addEventListener("install", function (event) {
